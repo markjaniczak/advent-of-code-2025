4edcf8bc8348572e55e483ee2f3f11129a2dcffb
Mark Janiczak <mark@janiczak.me>
2025-12-05 16:15:48 +0100

    fix day 3

--- day-3/script.js
+++ day-3/script.js
@@ -2,69 +2,43 @@ const fs = require("fs");
 const path = require("path");
 
 const FILE_PATH = process.argv[2];
+const IS_PART_TWO = process.argv[3] === "true";
 
-const BANKS = fs
-  .readFileSync(path.join(__dirname, FILE_PATH), "utf-8")
-  .trim()
-  .split("\n");
-
-function findHighestPair(joltages) {
-  const digits = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
-
-    const map = {};
-
-  for (let i = 0; i < joltages.length; i++) {
-    const digit = joltages[i];
-
-    if (!map[digit]) {
-      map[digit] = [];
-    }
-
-    map[digit].push(i);
-  }
-
-  for (let i = 0; i < digits.length; i++) {
-    const highDigit = digits[i];
-
-    if (!map[highDigit]) {
-      continue;
-    }
-
-    for (const leftIndex of map[highDigit]) {
-      for (let j = 0; j < digits.length; j++) {
-        const lowDigit = digits[j];
-
-        if (!map[lowDigit]) {
-          continue;
-        }
-
-        const indexes = map[lowDigit];
-
-        const right = indexes[indexes.length - 1];
-
-        if (right < leftIndex) {
-          continue;
-        }
-
-        for (const rightIndex of indexes) {
-          if (rightIndex > leftIndex) {
-            return parseInt(highDigit + lowDigit, 10);
-          }
-        }
-      }
-    }
-  }
-}
-
-function solution(banks) {
+function solution() {
   let total = 0;
+
+  const banks = fs
+    .readFileSync(path.join(__dirname, FILE_PATH), "utf-8")
+    .trim()
+    .split("\n");
+
+  const length = IS_PART_TWO ? 12 : 2;
 
   for (const bank of banks) {
     const joltages = bank.split("");
-    total += findHighestPair(joltages);
+
+    let digits = [];
+
+    while (joltages.length > 0) {
+      const digit = parseInt(joltages.shift(), 10);
+
+      while (
+        digits.length > 0 &&
+        digits[digits.length - 1] < digit &&
+        digits.length + joltages.length >= length
+      ) {
+        digits.pop();
+      }
+
+      if (digits.length < length) {
+        digits.push(digit);
+      }
+    }
+
+    total += parseInt(digits.join(""), 10);
   }
 
   return total;
 }
 
-console.log(solution(BANKS));
+console.log(solution());
